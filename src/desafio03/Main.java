d06cae31e7fb6ac0e58a01b97353c7b955e654a3
yanmullerwk <yan.muller.contact@gmail.com>
2025-08-26 15:56:44 -0300

    adicionei um condicional na saida do print salary adjustment

--- src/desafio03/Main.java
+++ src/desafio03/Main.java
@@ -27,7 +27,7 @@ public class Main {
     }
 
     public static float calculateSalaryAdjustment(float salary, String collaboratorLevel){
-        switch(collaboratorLevel){
+        switch(collaboratorLevel.toLowerCase()){
             case "silver":
                 return salary + (salary*BONUS_COLLABORATOR_LEVEL_SILVER);
             case "platinum":
@@ -35,11 +35,15 @@ public class Main {
             case "diamond":
                 return salary + (salary*BONUS_COLLABORATOR_LEVEL_DIAMONT);
             default:
-                return salary;
+                return -1;
         }
     }
 
     public static void printSalaryAdjustment(float salary, String collaboratorLevel, float salaryAdjustment){
-        System.out.println("The salary of the collaborator level " + collaboratorLevel + " was adjusted From R$" + salary + " To R$" + salaryAdjustment);
+        if(salaryAdjustment == -1){
+            System.out.println("collaborator level not identified");
+        }else{
+            System.out.println("The salary of the collaborator level " + collaboratorLevel + " was adjusted From R$" + salary + " To R$" + salaryAdjustment);
+        }
     }
 }
