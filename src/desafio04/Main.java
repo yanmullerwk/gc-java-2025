f75f88b8168e4c846a5e92f1a641135356212998
yanmullerwk <yan.muller.contact@gmail.com>
2025-09-03 22:33:36 -0300

    desafio numero 4

--- src/desafio04/Main.java
+++ src/desafio04/Main.java
@@ -63,7 +63,7 @@ public class Main {
     }
 
     public static void mostrarMenu() {
-        System.out.println("\n=== CALCULADORA ===");
+        System.out.println("\n=== CALCULADORA ;) ===");
         System.out.println("1 - Soma");
         System.out.println("2 - Subtração");
         System.out.println("3 - Multiplicação");
